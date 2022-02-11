c7e8b536834e5196f579dbcb4f61a6697583732c
BrahimAfa <brahimafassy@gmail.com>
2022-02-11 20:51:39 +0000

    Jenkinsfile and soe other changes related to models

--- src/main/java/com/ensa/domain/HistorySystem.java
+++ src/main/java/com/ensa/domain/HistorySystem.java
@@ -17,7 +17,6 @@ public class HistorySystem implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @NotNull
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
--- src/main/java/com/ensa/domain/Notification.java
+++ src/main/java/com/ensa/domain/Notification.java
@@ -16,7 +16,6 @@ public class Notification implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @NotNull
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
